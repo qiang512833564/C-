a8a22c33c61b292836b9a892d39e2ee9492a249a
HW <hw@zhangweideMacBook-Pro.local>
2016-08-02 10:53:54 +0800

    代码更新

---
+++ day03/TeamplateStudy/TeamplateStudy/TemplateOne.h
@@ -0,0 +1,13 @@
+//
+//  TemplateOne.h
+//  TeamplateStudy
+//
+//  Created by HW on 16/8/2.
+//  Copyright © 2016年 HW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TemplateOne : NSObject
+
+@end
---
+++ day03/TeamplateStudy/TeamplateStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TeamplateStudy
+//
+//  Created by HW on 16/8/2.
+//  Copyright © 2016年 HW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
